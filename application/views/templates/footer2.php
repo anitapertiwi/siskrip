f984b2339f38053747e91aea2728640f47bbaba1
faisalsyfl <faisalsyfl@gmail.com>
2017-12-21 20:29:32 +0700

    click diagram

--- application/views/templates/footer2.php
+++ application/views/templates/footer2.php
@@ -46,17 +46,17 @@
         };
 
         var chart = new google.visualization.ColumnChart(document.getElementById('columnchart_values'));
-          // function selectHandler() {
-          //   var selectedItem = chart.getSelection()[0];
-          //   if (selectedItem) {
-          //     var value = data.getValue(selectedItem.row, 0);
-          //     window.location = "<?php echo base_url(); ?>/Welcome/grafik/"+value;
-          //   }
-          // }
+          function selectHandler() {
+            var selectedItem = chart.getSelection()[0];
+            if (selectedItem) {
+              var value = data.getValue(selectedItem.row, 0);
+              window.location = "<?php echo base_url('Skripsi/index/'.$this->uri->segment(3)); ?>/"+value;
+            }
+          }
 
-          // // Listen for the 'select' event, and call my function selectHandler() when
-          // // the user selects something on the chart.
-          // google.visualization.events.addListener(chart, 'select', selectHandler);
+          // Listen for the 'select' event, and call my function selectHandler() when
+          // the user selects something on the chart.
+          google.visualization.events.addListener(chart, 'select', selectHandler);
           chart.draw(view, options);
           chart.draw(view, options);
           }
